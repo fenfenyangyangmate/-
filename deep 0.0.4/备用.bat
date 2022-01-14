df8ed8b5ccb968476c049a478dcad1ffe960334a
fenfenyangyangmate <90970696+fenfenyangyangmate@users.noreply.github.com>
2022-01-14 11:22:12 +0800

    Add files via upload

---
+++ deep 0.0.4/备用.bat
@@ -0,0 +1,1 @@
+python beiyong.py
---
+++ deep 0.0.4/测试.py
@@ -0,0 +1,8 @@
+import os,shutil
+os.getcwd()
+
+print(os.path.basename('C:/Users/46045/Desktop/821738.0.png'))
+
+print(((('Tags of E:\E\美图\八月\819737.0.jpg:').strip()).replace('Tags of ','')).strip(':'))
+
+shutil.copyfile('tag/三月.txt','新建文件夹/三月.txt')
