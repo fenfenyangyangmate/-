a7629b5f3bdb7ab78a2d48d6158371af929f8962
fenfenyangyangmate <90970696+fenfenyangyangmate@users.noreply.github.com>
2022-01-26 16:13:40 +0800

    Add files via upload

--- cs.py
+++ cs.py
@@ -6,42 +6,28 @@ from PIL import Image
 #By 魔王  （感兴趣可以加我qq：460452649）
 print('By 魔王  （感兴趣可以加我qq：460452649）')
 
-aaa=[]
 
-def found1(tags):
-    cursor = db.cursor()
-
-    tags=((tags.replace('(', '（')).replace(')', '）')).replace('/','$')
-    sql = f"SELECT * FROM {tags}"
-
-    # 执行SQL语句
-    cursor.execute(sql)
-    # 获取所有记录列表
-    results = cursor.fetchall()
-    for row in results:
-        id = row[0]
-        aaa.append(id)
-    print(aaa)
-    # except:
-    #     print('cuco')
-    # aaa.clear()
-
-if __name__ == '__main__':
+def writing(tag):
+    tag = ((tag.replace('(', '（')).replace(')', '）')).replace('/', '$')
+    aaa = 'F:/Grabber/gonoike_biwa/2000077.jpg'
     db = pymysql.connect(host='localhost',
                          user='root',
                          password='123456',
-                         database='tag')  # 这里一定要改
-    found1(input('ads'))
+                         database='tag')
+    cursor = db.cursor()
+    # SQL 插入语句
+    sql = f"INSERT INTO {tag} (FIRST_NAME) VALUES ('" + aaa + "')"
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+        print('cg')
+    except:
+        # 如果发生错误则回滚
 
-    # number = [1]
-    # mosi=[]
-    # # ccc=input('请选择模式，回车默认打开图片，任意输入只查询（查询结果在data/temporary.txt）：')
-    # while True:
-    #     db = pymysql.connect(host='localhost',
-    #                          user='root',
-    #                          password='123456',
-    #                      database='tag')      #这里一定要改
-    #     print('🔍' * 30)
-    #     t = input('请输入关键词（回车结束）：')
-    #     found1(t)
-
+        db.rollback()
+        pass
+    db.close()
+if __name__ == '__main__':
+    writing('xuanzang_(fate/grand_order)')
--- tag_storage.py
+++ tag_storage.py
@@ -19,7 +19,8 @@ def write():
         with open(f'extract/{i}', 'r', encoding='utf-8')as f1:
                 with open(f'data/data.txt', 'a', encoding='utf-8')as f3:
                     for line in f1.readlines():
-                        if 'Tags of' in line:
+                        a = line.strip()
+                        if 'Tags of' in a:
 
                             aaa=line.strip().replace('Tags of ','').strip(':').replace('\\','/')
                             if aaa in old_pic:
@@ -27,68 +28,48 @@ def write():
                             else:
                                 f3.write(aaa + '\n')
 
-                        elif '(' in line:
-                            a = line.strip('(')
-                            b = a.replace(')', '', 1)
-                            p1 = re.compile(r'[\(](.*)[\)]', re.S)
-                            for c in re.findall(p1, line):
-                                d = b.strip(c + ' ')
-                                e = d.strip('\n')
-                                if 'rating:' in e:
-                                    f = e.strip('rating:')
-                                    if f in tags:
-                                        continue
-                                    else:
-                                        tags.append(f.replace('-', '_'))
+                        elif a =='':
+                            pass
+
+                        else:
+
+                            p1 = re.compile(r'[\(](.*?)[\)]', re.S)
+                            b = a.replace((re.findall(p1, a))[0], '')
+                            c = b.replace('() ', '')
+                            if 'rating:' in c:
+                                f = c.replace('rating:', '')
+                                if f in tags:
+                                    continue
                                 else:
-                                    if e in tags:
-                                        continue
-                                    else:
-                                        tags.append(e.replace('-', '_'))
+                                    tags.append(f.replace('-', '_'))
+                            else:
+                                if c in tags:
+                                    continue
+                                else:
+                                    tags.append((c.replace('-', '_')).strip('‘'))
 
 
 
                     def writing(tag):
-                        aaaaaa = []
+                        tag = ((tag.replace('(', '（')).replace(')', '）')).replace('/', '$')
                         db = pymysql.connect(host='localhost',
                                              user='root',
                                              password='123456',
                                              database='tag')
-                        # 使用cursor()方法获取操作游标
-
                         cursor = db.cursor()
-                        # SQL 查询语句
-                        sql = f"SELECT * FROM {tag}"
+                        # SQL 插入语句
+                        sql = f"INSERT INTO {tag} (FIRST_NAME) VALUES ('" + aaa + "')"
                         try:
-                            # 执行SQL语句
+                            # 执行sql语句
                             cursor.execute(sql)
-                            # 获取所有记录列表
-                            results = cursor.fetchall()
-                            for row in results:
-                                id = row[0]
-                                aaaaaa.append(id)
-
-                            # if aaa in aaaaaa:
-                            #     # print(f'跳过 {aaa}')
-                            #     aaaaaa.clear()
-
-                            cursor = db.cursor()
-                            # SQL 插入语句
-                            sql = f"INSERT INTO {tag} (FIRST_NAME) VALUES ('" + aaa + "')"
-                            try:
-                                # 执行sql语句
-                                cursor.execute(sql)
-                                # 提交到数据库执行
-                                db.commit()
-                            except:
-                                # 如果发生错误则回滚
-
-                                db.rollback()
-                                pass
-                            db.close()
-
+                            # 提交到数据库执行
+                            db.commit()
                         except:
+                            # 如果发生错误则回滚
+
                             db.rollback()
+                            pass
+                        db.close()
 
                     # print(tags)
                     if aaa in old_pic:
