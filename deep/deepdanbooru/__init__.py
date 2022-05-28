0fbd6c99d4a7a11c74374762e0e98e2864c7bb82
fenfenyangyangmate <90970696+fenfenyangyangmate@users.noreply.github.com>
2022-05-28 19:12:10 +0800

    Delete deep directory

--- deep/deepdanbooru/__init__.py
+++
@@ -1,8 +0,0 @@
-import deepdanbooru.commands
-import deepdanbooru.data
-import deepdanbooru.extra
-import deepdanbooru.image
-import deepdanbooru.io
-import deepdanbooru.model
-import deepdanbooru.project
-import deepdanbooru.train
--- deep/deepdanbooru/project/__init__.py
+++
@@ -1,2 +0,0 @@
-from .project import DEFAULT_PROJECT_CONTEXT
-from .project import load_project
